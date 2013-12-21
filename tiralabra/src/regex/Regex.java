316f3b89f42906f962eb294426e26b0133cf206c
Jola <jola@Jola.(none)>
2013-12-21 14:30:26 +0200

    poistin roskakoodia

--- tiralabra/src/regex/Regex.java
+++ tiralabra/src/regex/Regex.java
@@ -1,60 +1,15 @@
 package regex;
 
-import finite_state_automaton.State;
-
 public class Regex {
     
     private String stringRep;
-    private State initState;
     
     public Regex(String stringRep) {
         this.stringRep = stringRep;
-        compileRegex();
     }
     
-    private void compileRegex() {
-        
-        int point = 0;
-        State curState = new State();
-        initState = curState;
-        State prevState = curState;
-        
-        while(point < stringRep.length()) {
-            switch(stringRep.charAt(point)) {
-                case '*':
-                    curState.addTransition(stringRep.charAt(point - 1),
-                            curState);
-                    prevState.setIsFinal(true);
-                    break;
-                    
-                default:
-                    State nextState = new State();
-                    curState.addTransition(stringRep.charAt(point),
-                            nextState);
-                    prevState = curState;
-                    curState = nextState;
-                    break; 
-            }
-            
-            point++;
-        }
-        
-        curState.setIsFinal(true);
-    }
+  
     
-    public Regex derive(char c) {
-        
-        if(stringRep.isEmpty())
-            return new Regex("\0");
-        
-        if(stringRep.length() == 1) {
-            if(stringRep.charAt(0) == c)
-                return new Regex("");
-            return new Regex("\0");
-        }
-        
-        return new Regex()
-    }
     
     private Regex[] splitRegex() {
         
@@ -106,21 +61,6 @@ public class Regex {
         return result;
     }
     
-    public boolean evalString(String s) {
-        
-        State curState = initState;
-        int point = 0;
-        
-        while(point < s.length() && curState != null) {
-            curState = curState.transfer(s.charAt(point));
-            point++;
-        }
-        
-        if(curState == null || point < s.length() - 1) {
-            return false;
-        }
-        
-        return curState.getIsFinal();
-    }
+
     
 }
